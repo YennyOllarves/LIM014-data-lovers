f03f4e97e3d338e9a726dc5748da568cd09933e1
Yennys <yenny1989@gmail.com>
2021-02-14 10:40:00 -0500

    prueba

--- src/data.js
+++ src/data.js
@@ -1,28 +1,20 @@
-// estas funciones son de ejemplo
 
-//export const example = () => {
-  //return 'example';
-//};
-
-//export const anotherExample = () => {
-  //return 'OMG';
-//};
-
-
-
-//export const anotherExample = () => {
-  //return 'OMG';
-//};
-
-export const gender = (datos) => {
-  let malePersonaje = datos.filter(personaje => personaje.gender == 'Male');
-  let femalePersonaje = datos.filter(personaje => personaje.gender == 'Female');
-  let undknownPersonaje = datos.filter(personaje => personaje.gender == 'unknown');
-  return (malePersonaje)
+//orderZ-A
+export const orderZA = (datos) => {
+  let ordenarZ = datos.sort((a,b) => {
+    if (a.name > b.name){
+      return -1
+    }
+    if (a.name > b.name){
+      return 1
+    }
+    return 0
+})
+return ordenarZ
 }
 
-export const order = (datos) => {
-  let ordenar = datos.sort((a,b) => {
+export const orderAZ = (datos) => {
+  let ordenarA = datos.sort((a,b) => {
     if (a.name < b.name){
       return -1
     }
@@ -31,5 +23,11 @@ export const order = (datos) => {
     }
     return 0
 })
-return ordenar
+return ordenarA
+}
+//Gender
+
+export const genderMale = (datos) => {
+  let malePersonaje = datos.filter(personaje => personaje.gender == 'Male');
+  return (malePersonaje)
 }
--- src/main.js
+++ src/main.js
@@ -1,28 +1,51 @@
-//import { example } from './data.js';
-// import data from './data/lol/lol.js';
-//import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-
-//console.log(example, data);
-
-import {gender} from './data.js';
-//import sonMale from './data.js';
-// import data from './data/lol/lol.js';
-//import data from './data/pokemon/pokemon.js';
 import data from './data/rickandmorty/rickandmorty.js';
-//import {order} from './data.js';
+
+import {orderAZ, orderZA, genderMale} from './data.js';
+
 
 const datos = data.results;
-//console.log(datos);
-console.log(gender(datos));
-//console.log(order(datos));
+const datoss = orderAZ(datos);
+//console.log(datoss);
+const datitos = orderZA(datos);
+//console.log(datitos);
+const genderM = genderMale(datos);
 
-//let list = document.querySelectorAll('.submenu');
-//console.log(list);
+const items=document.getElementById('items');
+const genderr=document.getElementById('genderr');
+const templateCard = document.getElementById('template-card').content;
+//const templateCardFront = document.getElementById('template-card-front').content;
+const fragment = document.createDocumentFragment();
+
+const pintarCards = datos => {
+    datos.forEach(personaje => {
+        //console.log(personaje)
+        templateCard.querySelector('h3').textContent = personaje.name
+        templateCard.querySelector('img').setAttribute("src", personaje.image)
+        templateCard.querySelector('h4').textContent = personaje.status
+        templateCard.querySelector('h5').textContent = personaje.species
+        templateCard.querySelector('p').textContent = personaje.gender
+        
+        const clone = templateCard.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+
+    items.appendChild(fragment)
+    genderr.appendChild(fragment)
+}
+
+pintarCards(datos, genderM)
+console.log(datos, genderM, datoss)
+
+
+
+
+
+
 document.querySelectorAll('.submenu').forEach(item => {
     item.addEventListener('click', event => {
         document.getElementById('segundapagina').style.display = 'block'
         document.getElementById('paginaprincipal').style.display = 'none';
+        document.getElementById('tercerapagina').style.display='block';
     //Write your code hear
     console.log(event);
     })
