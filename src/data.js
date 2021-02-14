95c40e295fc1549ff1bb60ef4df0d500ff8a205b
RocioValentin <rociovcl.10@gmail.com>
2021-02-14 10:13:32 -0500

    Avance de los filtros de los personajes

--- src/data.js
+++ src/data.js
@@ -1,27 +1,5 @@
-// estas funciones son de ejemplo
-
-//export const example = () => {
-  //return 'example';
-//};
-
-//export const anotherExample = () => {
-  //return 'OMG';
-//};
-
-
-
-//export const anotherExample = () => {
-  //return 'OMG';
-//};
-
-export const gender = (datos) => {
-  let malePersonaje = datos.filter(personaje => personaje.gender == 'Male');
-  let femalePersonaje = datos.filter(personaje => personaje.gender == 'Female');
-  let undknownPersonaje = datos.filter(personaje => personaje.gender == 'unknown');
-  return (malePersonaje)
-}
-
-export const order = (datos) => {
+//orderA-Z
+export const orderAZ = (datos) => {
   let ordenar = datos.sort((a,b) => {
     if (a.name < b.name){
       return -1
@@ -33,3 +11,63 @@ export const order = (datos) => {
 })
 return ordenar
 }
+
+//orderZ-A
+export const orderZA = (datos) => {
+  let ordenar = datos.sort((a,b) => {
+    if (a.name > b.name){
+      return -1
+    }
+    if (a.name > b.name){
+      return 1
+    }
+    return 0
+})
+return ordenar
+}
+
+//Gender
+
+export const genderMale = (datos) => {
+  let malePersonaje = datos.filter(personaje => personaje.gender == 'Male');
+  return (malePersonaje)
+}
+export const genderFemale = (datos) => {
+  let femalePersonaje = datos.filter(personaje => personaje.gender == 'Female');
+  return (femalePersonaje)
+}
+export const genderUnknown = (datos) => {
+  let unknownPersonaje = datos.filter(personaje => personaje.gender == 'unknown');
+  return (unknownPersonaje)
+} 
+
+//Species
+
+export const speciesHuman = (datos) => {
+  let humanPersonaje = datos.filter(personaje => personaje.species == 'Human');
+  return (humanPersonaje)
+} 
+
+export const speciesAlien = (datos) => {
+  let alienPersonaje = datos.filter(personaje => personaje.species == 'Alien');
+  return (alienPersonaje)
+} 
+
+//Status
+
+export const statusAlive = (datos) => {
+  let alivePersonaje = datos.filter(personaje => personaje.status == 'Alive');
+  return (alivePersonaje)
+} 
+
+export const statusDead = (datos) => {
+  let deadPersonaje = datos.filter(personaje => personaje.status == 'Dead');
+  return (deadPersonaje)
+} 
+
+//Personajes de la Tierra 
+
+export const tierraOrigin = (datos) => {
+  let tierraPersonaje = datos.filter(personaje => personaje.origin.name == 'Earth (C-137)');
+  return (tierraPersonaje)
+} 
